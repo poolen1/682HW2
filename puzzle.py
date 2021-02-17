0de12f6d1e8f6504a9b00cc3f58ee76863bf16be
poolen108 <poolen108@gmail.com>
2021-02-17 07:30:39 -0800

    puzzle places random numbers in spaces. TODO: range(0,15), no repeats

--- puzzle.py
+++ puzzle.py
@@ -1,10 +1,15 @@
+import numpy as np
+
 
 class Puzzle:
     def __init__(self, board=None):
         self.rows = 4
         self.cols = 4
-        self.board = [[] * self.cols] * self.rows
+        self.board = [[0] * self.cols] * self.rows
         if board is None:
-            pass  # init board with rand
+            for row in range(self.rows):
+                for col in range(self.cols):
+                    n = np.random.randn()
+                    self.board[row][col] = n
         else:
             self.board = board
