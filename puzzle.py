e3096c7e264ee36b0a7ec769deaed684ced8fc14
poolen108 <poolen108@gmail.com>
2021-02-17 06:20:36 -0800

    added puzzle class

--- puzzle.py
+++ puzzle.py
@@ -3,5 +3,8 @@ class Puzzle:
     def __init__(self, board=None):
         self.rows = 4
         self.cols = 4
+        self.board = [[] * self.cols] * self.rows
         if board is None:
-            self.board = [[]*self.cols]*self.rows
+            pass  # init board with rand
+        else:
+            self.board = board
