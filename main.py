3286869ccc44c40a19b440aa7267c2c4a83de504
poolen108 <poolen108@gmail.com>
2021-02-21 09:33:44 -0800

    check for solved board

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import puzzle
 
-f = open("./boards/board1.txt", "r")
+# f = open("./boards/board1.txt", "r")
+# f = open("./boards/41invBoard.txt", "r")
+# f = open("./boards/62invBoard.txt", "r")
+f = open("./boards/solvedBoard.txt", "r")
 
 p = puzzle.Puzzle(f)
 # p = puzzle.Puzzle()
 
 print(p.board)
-print(p.player_pos_row, p.player_pos_col)
-# print(p.is_solved())
+# print(p.player_pos_row, p.player_pos_col)
+print("is solved: ", p.is_solved())
 
-p.move_right()
-print(p.board)
-print(p.player_pos_row, p.player_pos_col)
+# p.move_right()
+#  print(p.board)
+#  print(p.player_pos_row, p.player_pos_col)
--- puzzle.py
+++ puzzle.py
@@ -10,6 +10,30 @@ class Puzzle:
         self.player_pos_col = 0
         self.player_pos_row = 0
 
+        solvable = False
+        while not solvable:
+            self.init_board(file)
+            # Get player pos
+            self.player_pos_row, self.player_pos_col = \
+                self.get_player_position()
+            # Check for solvability
+            solvable = self.is_solvable()
+            print("is solvable: ", solvable)
+            if (file is not None) & solvable is False:
+                exit()
+
+        # Get solution
+        i = 1
+        for col in range(self.cols):
+            solution_row = []
+            for row in range(self.rows):
+                solution_row.append(str(i))
+                i += 1
+            self.solution.append(solution_row)
+        self.solution[3][3] = '0'
+        print(self.solution)
+
+    def init_board(self, file):
         # Set up board randomly
         if file is None:
             n = np.random.choice(range(16), 16, replace=False)
@@ -25,18 +49,6 @@ class Puzzle:
             for line in file.readlines():
                 self.board.append(line.split())
 
-        # Get player pos
-        self.player_pos_row, self.player_pos_col = self.get_player_position()
-
-        # Get solution
-        i = 0
-        for col in range(self.cols):
-            solution_row = []
-            for row in range(self.rows):
-                solution_row.append('i')
-                i += 1
-            self.solution.append(solution_row)
-
     def get_player_position(self):
         for col in range(self.cols):
             for row in range(self.rows):
@@ -44,6 +56,40 @@ class Puzzle:
                     pos_row = col
                     pos_col = row
                     return pos_row, pos_col
+
+    def is_solvable(self):
+        solvable = False
+        inv_count = self.count_inversions()
+        print("inversions: ", inv_count)
+        print("pos row: ", self.player_pos_row % 2)
+        if self.player_pos_row % 2 == 0:  # Empty pos is on even row
+            # odd number of inversions
+            # print ("inv_count: ", inv_count % 2)
+            if inv_count % 2 == 1:
+                solvable = True
+        else:  # Empty pos is on odd row
+            # even number of inversions
+            if inv_count % 2 == 0:
+                solvable = True
+        # print("is_solvable: ", solvable)
+        return solvable
+
+    def count_inversions(self):
+        inv_count = 0
+        arr = []
+        for idn, n in np.ndenumerate(self.board):
+            arr.append(n)
+        for i in range(len(arr)):
+            # print("i: ", i)
+            if arr[i].astype(np.int) == 0:
+                continue
+            for j in range(i + 1, 16):
+                # print("i, j, n, m: ", i, j, arr[i], arr[j])
+                if arr[j].astype(np.int) == 0:
+                    continue
+                if arr[i] > arr[j]:
+                    inv_count += 1
+        return inv_count
 
     def is_solved(self):
         if self.board == self.solution:
