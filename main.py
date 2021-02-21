37487be89f41627bedf633e6a610b34376846f79
poolen108 <poolen108@gmail.com>
2021-02-20 20:14:10 -0800

    operators added

--- main.py
+++ main.py
@@ -6,4 +6,9 @@ p = puzzle.Puzzle(f)
 # p = puzzle.Puzzle()
 
 print(p.board)
-print(p.player_position)
+print(p.player_pos_row, p.player_pos_col)
+# print(p.is_solved())
+
+p.move_right()
+print(p.board)
+print(p.player_pos_row, p.player_pos_col)
--- puzzle.py
+++ puzzle.py
@@ -7,7 +7,8 @@ class Puzzle:
         self.cols = 4
         self.board = []
         self.solution = []
-        self.player_position = (0, 0)
+        self.player_pos_col = 0
+        self.player_pos_row = 0
 
         # Set up board randomly
         if file is None:
@@ -23,31 +24,75 @@ class Puzzle:
         else:
             for line in file.readlines():
                 self.board.append(line.split())
-        # Get player pos
-        self.player_position = self.get_player_position()
-        # Get solution
 
+        # Get player pos
+        self.player_pos_row, self.player_pos_col = self.get_player_position()
+
+        # Get solution
+        i = 0
+        for col in range(self.cols):
+            solution_row = []
+            for row in range(self.rows):
+                solution_row.append('i')
+                i += 1
+            self.solution.append(solution_row)
 
     def get_player_position(self):
         for col in range(self.cols):
             for row in range(self.rows):
                 if self.board[col][row] == '0':
-                    pos = (col, row)
-                    return pos
+                    pos_row = col
+                    pos_col = row
+                    return pos_row, pos_col
 
     def is_solved(self):
-        pass
+        if self.board == self.solution:
+            return True
+        else:
+            return False
 
     def move_up(self):
-        pass
+        posrow = self.player_pos_row
+        poscol = self.player_pos_col
+        if self.player_pos_row == 0:
+            return print("Illegal move")
+        else:
+            self.board[posrow][poscol], self.board[posrow - 1][poscol]\
+                = self.board[posrow - 1][poscol], self.board[posrow][poscol]
+            self.player_pos_row, self.player_pos_col \
+                = self.get_player_position()
 
     def move_down(self):
-        pass
+        posrow = self.player_pos_row
+        poscol = self.player_pos_col
+        if self.player_pos_row == 3:
+            return print("Illegal move")
+        else:
+            self.board[posrow][poscol], self.board[posrow + 1][poscol] \
+                = self.board[posrow + 1][poscol], self.board[posrow][poscol]
+            self.player_pos_row, self.player_pos_col \
+                = self.get_player_position()
 
     def move_left(self):
-        pass
+        posrow = self.player_pos_row
+        poscol = self.player_pos_col
+        if self.player_pos_col == 0:
+            return print("Illegal move")
+        else:
+            self.board[posrow][poscol], self.board[posrow][poscol - 1] \
+                = self.board[posrow][poscol - 1], self.board[posrow][poscol]
+            self.player_pos_row, self.player_pos_col \
+                = self.get_player_position()
 
     def move_right(self):
-        pass
+        posrow = self.player_pos_row
+        poscol = self.player_pos_col
+        if self.player_pos_col == 3:
+            return print("Illegal move")
+        else:
+            self.board[posrow][poscol], self.board[posrow][poscol + 1] \
+                = self.board[posrow][poscol + 1], self.board[posrow][poscol]
+            self.player_pos_row, self.player_pos_col \
+                = self.get_player_position()
 
 
