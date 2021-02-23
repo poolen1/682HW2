64fb6ad1ec90605320e10b19694ee85ed9571266
poolen108 <poolen108@gmail.com>
2021-02-23 10:25:27 -0800

    works but slow

--- a_star.py
+++ a_star.py
@@ -30,47 +30,92 @@ class AStarSearch:
         self.nodes_visited = 0
 
     def expand_node(self, bestnode):
+        # print("expand successors: ", bestnode.successors)
+        if bestnode.successors:
+            self.closed_list.append(bestnode)
+            return
         moves = self.get_legal_ops(bestnode)
 
         # for succ in bestnode.successors:
-        #     print("first", succ.state, succ.f)
+        #     # print("first", succ.state, succ.f)
+        # bestnode.successors = []
+
+        # # print("start successors: ", len(bestnode.successors))
 
         for item in moves:
-            # print(item)
+            # # print(item)
             new_node = self.create_successor(bestnode)
 
             if item == "up":
+                # print('up')
                 new_node.state = new_node.state.move_up()
             elif item == 'down':
+                # print('down')
                 new_node.state = new_node.state.move_down()
             elif item == "left":
+                # print('left')
                 new_node.state = new_node.state.move_left()
             elif item == "right":
+                # print('right')
                 new_node.state = new_node.state.move_right()
 
             is_dupe = self.is_dupe(new_node)
+            # # print(is_dupe)
             if is_dupe:
-                new_node = is_dupe
+                if is_dupe[2] == 'open':
+                    old = is_dupe[1]
+                    if new_node.g > old.g:
+                        # print("open dupe better path: ", new_node.g, old.g)
+                        old.ptr = new_node.ptr
+                        new_node = old
+                    self.open_list.remove(old)
+                    # print("removed old from open")
+                elif is_dupe[2] == 'closed':
+                    old = is_dupe[1]
+                    # # print("old")
+                    if new_node.g > old.g:
+                        # print("closed dupe better path: ", new_node.g, old.g)
+                        old.ptr = new_node.ptr
+                        new_node = old
+                        # self.propagate_closed_old(old)
+                    self.closed_list.remove(old)
+                    # print("removed old from closed")
 
             new_node.h = self.manhattan(new_node)
             new_node.f = new_node.g + new_node.h
-            # print(new_node.state, new_node.f)
+            # # print(new_node.state, new_node.f)
 
-            bestnode.successors.append(new_node)
+            valid_successor = True
+            if bestnode.successors:
+                valid_successor = False
+                for node in bestnode.successors:
+                    if new_node.state.board == node.state.board:
+                        if new_node.f < node.f:
+                            bestnode.successors.remove(node)
+                            valid_successor = True
+                    else:
+                        valid_successor = True
+
+            # print(bestnode.successors)
+            # # print('successor count', len(bestnode.successors))
+            # print(valid_successor)
+
+            if valid_successor:
+                bestnode.successors.append(new_node)
 
         self.open_list += bestnode.successors
 
         # bestnode = self.choose_bestnode()
         # for item in self.open_list:
-        #     print(item.state.board)
-        #     print(item.f)
+        #     # print(item.state.board)
+        #     # print(item.f)
         # exit()
 
     @staticmethod
     def create_successor(node):
         new_node = AStarNode(node.state, node, node.g + 1, node.h)
         new_node.successors = []
-        # print(new_node.state, new_node.g)
+        # # print(new_node.state, new_node.g)
         return new_node
 
     @staticmethod
@@ -130,13 +175,41 @@ class AStarSearch:
                 h += abs(solcol - col)
                 h += abs(solrow - row)
         h *= 10
-        # print(h)
+        # # print(h)
         return h
 
     def choose_bestnode(self):
         self.open_list.sort(key=lambda x: x.f, reverse=True)
+        dupecount = 0
+        closedcount = 0
+        open_length = len(self.open_list)
+        closed_length = len(self.closed_list)
+        open_remove_list = []
+        closed_remove_list = []
         bestnode = self.open_list.pop()
-        self.nodes_visited += 1
+        if bestnode.successors:
+            low = bestnode
+            for node in bestnode.successors:
+                if node.f < low.f:
+                    low = node
+            if low == bestnode:
+                bestnode = self.choose_bestnode()
+            else:
+                bestnode = low
+        # print("========")
+        # # print("Bestnode Successors: ", bestnode.successors)
+        # # print("Count: ", len(bestnode.successors))
+        for i in range(0, len(self.open_list)):
+            if bestnode.state.board == self.open_list[i].state.board:
+                dupecount += 1
+        for i in range(0, len(self.closed_list)):
+            if bestnode.state.board == self.closed_list[i].state.board:
+                closedcount += 1
+        # print("bestnode: ", bestnode.f)
+        # print("open dupecount: ", dupecount)
+        # print("closed dupecount: ", closedcount)
+        # print("========")
+        self.prune(bestnode)
         self.closed_list.append(bestnode)
         return bestnode
 
@@ -144,13 +217,18 @@ class AStarSearch:
         solved = False
         while solved is False:
             if not self.open_list:
-                print("Failure")
+                # print("Failure")
                 exit()
             bestnode = self.choose_bestnode()
+            self.nodes_visited += 1
+            # print("nodes visited: ", self.nodes_visited)
+            # print("open list: ", len(self.open_list))
             # ("bestnode: ", bestnode.state, bestnode.f)
             if bestnode.state.is_solved(self.end_node.state.board):
+                # print("solved")
                 solved = True
                 continue
+            # print("to expand successors: ", bestnode.successors)
             self.expand_node(bestnode)
 
         print("Puzzle solved")
@@ -158,31 +236,25 @@ class AStarSearch:
         print("Nodes visited: ", self.nodes_visited)
         print(self.start_node.state.board)
         print(bestnode.state.board)
-        exit()
+        return
 
     def is_dupe(self, node):
         duplicate = False
         for open_node in self.open_list:
             if node.state.board == open_node.state.board:
-                # print(self.nodes_visited)
+                # print("open dupe: ", self.nodes_visited)
                 duplicate = True
-                if node.g > open_node.g:
-                    open_node.ptr = node.ptr
-                    node.ptr = ""
-                return open_node
+                return duplicate, open_node, 'open'
 
         for closed_node in self.closed_list:
             if node.state.board == closed_node.state.board:
-                # print('nodes visited: ', self.nodes_visited)
+                # print('closed dupe: ', self.nodes_visited)
                 duplicate = True
-                if node.g > closed_node.g:
-                    closed_node.ptr = node.ptr
-                    node.ptr = ""
-                    self.propagate_closed_old(closed_node)
-                return closed_node
+                return duplicate, closed_node, 'closed'
         return duplicate
 
-    def propagate_closed_old(self, old):
+    @staticmethod
+    def propagate_closed_old(old):
         start_node = DFSNode(old, None, 0)
         d_limit = 1000
         open_list = [start_node]
@@ -190,22 +262,37 @@ class AStarSearch:
         while True:
             if not open_list:
                 return
-            # print(open_list)
+            # # print(open_list)
             n = open_list.pop(0)
-            # print(n)
+            # # print(n)
             closed_list.append(n)
 
-            if n.d <= d_limit:
-                if n.state.successors:
-                    for successor in n.state.successors:
-                        if successor.ptr == n.state or successor.g > n.state.g:
+            if n.d <= d_limit:  # d is less than Depth limit
+                if n.state.successors:  # if n has successors
+                    for successor in n.state.successors:  # for each successor
+                        # if successor points to n, or successor is more expensive,
+                        # update new node and continue propagation
+                        if successor.ptr == n.state or successor.g < n.state.g:
                             new_node = DFSNode(successor, n, n.d + 1)
                             new_node.state.g = n.state.g + 1
                             new_node.state.f = new_node.state.g + new_node.state.h
                             open_list.append(new_node)
 
-    def prune(self):
-        pass
+    def prune(self, bestnode):
+        cutoff_value = bestnode.f * 10
+        cutoff_index = 0
+        maxf = 0
+        for i in range(0, len(self.open_list)):
+            node = self.open_list[i]
+            if node.f > maxf:
+                maxf = node.f
+            if node.f >= cutoff_value:
+                print("cutoff: ", node.f)
+                cutoff_index = i
+                del self.open_list[cutoff_index:]
+                break
+        # print("bestnode: ", bestnode.f)
+        # print('max: ', maxf)
 
     def get_path(self):
         pass
--- main.py
+++ main.py
@@ -1,40 +1,24 @@
 import puzzle
 import a_star
+import timeit
+from datetime import datetime
 
-# f = open("./boards/board1.txt", "r")
-# f = open("./boards/spiralBoard.txt", "r")
-# f = open("./boards/backwardBoard.txt", "r")
-# f = open("./boards/41invBoard.txt", "r")
-# f = open("./boards/62invBoard.txt", "r")
-# f = open("./boards/solvedBoard.txt", "r")
-# f = open("./boards/56invBoard.txt", "r")
-# f = open("./boards/easyBoard.txt", "r")
+f1 = open("./boards/board1.txt", "r")
+f2 = open("./boards/spiralBoard.txt", "r")
+f3 = open("./boards/backwardBoard.txt", "r")
 
-p = puzzle.Puzzle(f)
-# p = puzzle.Puzzle()
+def run(file):
+    f = file
+    p = puzzle.Puzzle(f)
+    sol = p.init_solution()
+    search = a_star.AStarSearch(p, sol)
+    start = datetime.now()
+    search.search()
+    stop = datetime.now()
+    print("runtime: ", stop - start)
+    print('===================================')
 
-sol = p.init_solution()
 
-search = a_star.AStarSearch(p, sol)
-search.expand_node(search.start_node)
-
-# print(p.board)
-# print(p.player_pos_row, p.player_pos_col)
-# print(sol.board)
-# print(sol.player_pos_row, sol.player_pos_col)
-# print("is solved: ", p.is_solved(sol.board))
-
-search.search()
-
-# p.move_right()
-#  print(p.board)
-#  print(p.player_pos_row, p.player_pos_col)
-
-# p_node = p.move_up()
-# print(p_node.board)
-# p_node = p_node.move_right()
-# print(p_node.board)
-# p_node = p_node.move_down()
-# print(p_node.board)
-# p_node = p_node.move_left()
-# print(p_node.board)
+# run(f1)
+run(f2)
+# run(f3)
